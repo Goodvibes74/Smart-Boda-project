7fdb05bc646cd706e342b9d7cf38bfd9585f872f
Goodvibes74 <kazibwed499@gmail.com>
2025-07-02 00:38:13 +0300

    Add initial HTML and manifest files for safe_buddy_ver2 web app
    - Created index.html with necessary meta tags, scripts, and base href for Flutter.
    - Added manifest.json to define app properties, icons, and display settings.

--- safe_buddy_version_2/safe_buddy_ver2/lib/main.dart
+++ safe_buddy_version_2/safe_buddy_ver2/lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  // Ensure that Firebase is initialized before running the app
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // Run the app
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   // This widget is the root of your application.
   @override
