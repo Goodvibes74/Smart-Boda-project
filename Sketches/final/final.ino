ce05ba099f907bca2b87787efbfc11c78798b37c
Mawande John PAul <johnpauljones181103@gmail.com>
2025-07-02 17:30:14 +0300

    Update final.ino

--- Sketches/final/final.ino
+++ Sketches/final/final.ino
@@ -1,31 +1,42 @@
 #include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
 
-#define GPS_RX 32  // GPS TX to ESP32 RX
-#define GPS_TX 34  // GPS RX to ESP32 TX
-#define GSM_RX 17  // GSM TX to ESP32 RX
-#define GSM_TX 16  // GSM RX to ESP32 TX
+// --------- Function Prototypes ---------
+void sendGSM(String command);
+void getGPS();
+void getAccelerometer();
+void sendToThingSpeak(String payload);
 
-SoftwareSerial gpsSerial(GPS_RX, GPS_TX);
-SoftwareSerial gsmSerial(GSM_RX, GSM_TX);
+// --------- Pin Definitions ---------
+#define GPS_RX 32
+#define GPS_TX 34
+#define GSM_RX 17
+#define GSM_TX 16
 
+// --------- Serial Interfaces ---------
+// Use available hardware serial ports
+HardwareSerial gpsSerial(1); // UART1
+HardwareSerial gsmSerial(2); // UART2
+
+// --------- Global Variables ---------
 Adafruit_MPU6050 mpu;
+TinyGPSPlus gps;
 
-String gpsData = "";
+float latitude = 0;
+float longitude = 0;
 float ax, ay, az;
 
+// --------- Setup ---------
 void setup() {
   Serial.begin(115200);
   Wire.begin();
 
-  // Initialize GPS
-  gpsSerial.begin(9600);
+  gpsSerial.begin(9600, SERIAL_8N1, GPS_RX, GPS_TX);
   Serial.println("GPS Serial started");
 
-  // Initialize GSM
-  gsmSerial.begin(9600);
+  gsmSerial.begin(9600, SERIAL_8N1, GSM_RX, GSM_TX);
   Serial.println("GSM Serial started");
 
   // Initialize MPU6050
@@ -36,40 +47,52 @@ void setup() {
   Serial.println("MPU6050 Found!");
 
   delay(2000);
-  sendGSM("AT"); // Init check
-  sendGSM("AT+CSQ"); // Signal quality
+
+  // Setup GSM Internet
+  sendGSM("AT");
+  sendGSM("AT+CSQ");
   sendGSM("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"");
-  sendGSM("AT+SAPBR=3,1,\"APN\",\"internet\""); // Use your GSM carrier APN
-  sendGSM("AT+SAPBR=1,1"); // Open bearer
+  sendGSM("AT+SAPBR=3,1,\"APN\",\"internet\""); // Use your SIM's APN
+  sendGSM("AT+SAPBR=1,1");
 }
 
+// --------- Main Loop ---------
 void loop() {
   getGPS();
   getAccelerometer();
 
-  String payload = "field1=" + String(ax) + "&field2=" + String(ay) +
-                   "&field3=" + String(az) + "&field4=" + gpsData;
+  String payload = "field1=" + String(ax) +
+                   "&field2=" + String(ay) +
+                   "&field3=" + String(az) +
+                   "&field4=" + String(latitude, 6) +
+                   "&field5=" + String(longitude, 6);
 
   sendToThingSpeak(payload);
-
-  delay(15000); // ThingSpeak limit: 15s
+  delay(15000); // ThingSpeak requires 15 sec interval
 }
 
+// --------- Get GPS Location ---------
 void getGPS() {
-  gpsData = "";
-  unsigned long startTime = millis();
-  while (millis() - startTime < 2000) {
-    if (gpsSerial.available()) {
-      char c = gpsSerial.read();
-      gpsData += c;
+  unsigned long start = millis();
+  while (millis() - start < 2000) {
+    while (gpsSerial.available()) {
+      gps.encode(gpsSerial.read());
     }
   }
 
-  // Optionally, parse NMEA sentences here if needed
-  Serial.println("GPS Raw Data:");
-  Serial.println(gpsData);
+  if (gps.location.isValid()) {
+    latitude = gps.location.lat();
+    longitude = gps.location.lng();
+    Serial.print("Latitude: ");
+    Serial.println(latitude, 6);
+    Serial.print("Longitude: ");
+    Serial.println(longitude, 6);
+  } else {
+    Serial.println("Waiting for valid GPS fix...");
+  }
 }
 
+// --------- Read MPU6050 Acceleration ---------
 void getAccelerometer() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
@@ -84,6 +107,7 @@ void getAccelerometer() {
   Serial.print("Z: "); Serial.println(az);
 }
 
+// --------- Send AT Commands to GSM ---------
 void sendGSM(String command) {
   gsmSerial.println(command);
   delay(1000);
@@ -92,12 +116,12 @@ void sendGSM(String command) {
   }
 }
 
+// --------- Send Data to ThingSpeak ---------
 void sendToThingSpeak(String payload) {
   sendGSM("AT+HTTPINIT");
   sendGSM("AT+HTTPPARA=\"CID\",1");
-  sendGSM("AT+HTTPPARA=\"URL\",\"http://api.thingspeak.com/update?api_key= "GEOB8L3MARYBEO01" + payload + "\"");
+  sendGSM("AT+HTTPPARA=\"URL\",\"http://api.thingspeak.com/update?api_key=GEOB8L3MARYBEO01&" + payload + "\"");
   sendGSM("AT+HTTPACTION=0");
-
   delay(1000);
   sendGSM("AT+HTTPTERM");
 }
